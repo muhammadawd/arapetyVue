1782ea482b7a67580a8c6db35c8aad1095679750
unknown <devmohamedawd@gmail.com>
2020-06-10 09:55:15 +0200

    'recode'

--- src/views/modules/map/Index.vue
+++ src/views/modules/map/Index.vue
@@ -149,7 +149,7 @@
             vm.center = locations[0];
           }
           let center = vm.center;
-
+          vm.locations = locations;
           vm.mapSetCenter(center);
         })
       },
